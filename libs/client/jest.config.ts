7aab5636966d1f261d64d9fe0825165ed049121e
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-05-08 17:04:30 +0200

    Fix jest config

--- libs/client/jest.config.ts
+++ libs/client/jest.config.ts
@@ -1,23 +1,9 @@
 /* eslint-disable */
-import { readFileSync } from "fs";
-
-// Reading the SWC compilation config and remove the "exclude"
-// for the test files to be compiled by SWC
-const { exclude: _, ...swcJestConfig } = JSON.parse(
-  readFileSync(`${__dirname}/.swcrc`, "utf-8")
-);
-
-// disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves.
-// If we do not disable this, SWC Core will read .swcrc and won't transform our test files due to "exclude"
-if (swcJestConfig.swcrc === undefined) {
-  swcJestConfig.swcrc = false;
-}
-
 export default {
   displayName: "client",
   preset: "../../jest.preset.js",
   transform: {
-    "^.+\\.[tj]s$": ["@swc/jest", swcJestConfig],
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/libs/client",
