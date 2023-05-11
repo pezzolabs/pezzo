936ae5b0611fc82c50c4bec363f52d0053759e13
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-05-11 15:22:38 +0200

    Fix formatting

--- apps/server/src/ai21-test.ts
+++ apps/server/src/ai21-test.ts
@@ -1,26 +1,26 @@
-// import { Pezzo } from "@pezzo/client";
-// import  AI21Integration from "@pezzo/integrations/src/lib/integrations/ai21";
+import { Pezzo } from "@pezzo/client";
+import  AI21Integration from "@pezzo/integrations/lib/integrations/ai21";
 
-// const apiKey = process.env["AI21_API_KEY"];
+const apiKey = process.env["AI21_API_KEY"];
 
-// if (!apiKey) {
-//   throw new Error("AI21_API_KEY is not set");
-// }
+if (!apiKey) {
+  throw new Error("AI21_API_KEY is not set");
+}
 
-// async function main() {
-//   const pezzo = new Pezzo({
-//     pezzoServerURL: "http://localhost:3000",
-//     environment: "development",
-//   });
+async function main() {
+  const pezzo = new Pezzo({
+    pezzoServerURL: "http://localhost:3000",
+    environment: "development",
+  });
 
-//   const ai21 = new AI21Integration.Executor(pezzo, {
-//     apiKey,
-//   });
+  const ai21 = new AI21Integration.Executor(pezzo, {
+    apiKey,
+  });
 
-//   const { result } = await ai21.run("CreateTasks", {
-//     goal: "Renovate my bathroom",
-//   });
-//   console.log("result", result);
-// }
+  const { result } = await ai21.run("CreateTasks", {
+    goal: "Renovate my bathroom",
+  });
+  console.log("result", result);
+}
 
-// main();
+main();
