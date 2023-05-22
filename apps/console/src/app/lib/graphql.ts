04467a5e311d7e7b045751e6c12a72654dbea62b
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-05-22 19:38:22 +0200

    remove rudundant fetchData function

--- apps/console/src/app/lib/graphql.ts
+++ apps/console/src/app/lib/graphql.ts
@@ -6,18 +6,10 @@ import {
   signOut,
 } from "supertokens-auth-react/recipe/session";
 
-async function fetchData(url, options) {
-  try {
-    return fetch(url, options);
-  } catch (error) {
-    throw error;
-  }
-}
-
 export const gqlClient = new GraphQLClient(`${BASE_API_URL}/graphql`, {
   credentials: "include",
   fetch: async (url, options) => {
-    const res = await fetchData(url, options);
+    const res = await fetch(url, options);
     const json = await res.clone().json();
 
     if (json.errors && json.errors.length > 0) {
@@ -25,15 +17,15 @@ export const gqlClient = new GraphQLClient(`${BASE_API_URL}/graphql`, {
       if (json.errors[0].extensions?.code === "UNAUTHORIZED") {
         // Attempt to refresh the session
         const isSuccessful = await attemptRefreshingSession();
-  
+
         if (!isSuccessful) {
           signOut();
           window.location.href = "/login";
           return;
         }
-  
+
         // Retry request
-        return fetchData(url, options);
+        return fetch(url, options);
       }
     }
 
