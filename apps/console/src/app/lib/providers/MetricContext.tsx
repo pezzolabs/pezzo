10352f9ad3b7a323c4ff4abca3415c78a41f98b7
Ayan Das <dasayan348@gmail.com>
2023-05-22 15:12:03 +0530

    Added control in dashboard metrics

--- apps/console/src/app/lib/providers/MetricContext.tsx
+++ apps/console/src/app/lib/providers/MetricContext.tsx
@@ -85,44 +85,42 @@ export const MetricProvider = ({
       <Card style={{ width: "100%" }}>
         <div style={{ marginBottom: token.marginMD }}>
           <Typography.Title level={4}>{title}</Typography.Title>
+          <Row style={{ marginBottom: token.marginLG }}>
+            <Col span={12}>
+              <Select
+                defaultValue={start}
+                style={{ width: 140 }}
+                onSelect={setStart}
+              >
+                <Select.Option value="-1h">Past Hour</Select.Option>
+                <Select.Option value="-1d">Past Day</Select.Option>
+                <Select.Option value="-7d">Past 7 days</Select.Option>
+                <Select.Option value="-30d">Past 30 days</Select.Option>
+                <Select.Option value="-1y">Past year</Select.Option>
+              </Select>
+            </Col>
+            <Col
+              span={12}
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Radio.Group
+                value={granularity}
+                onChange={(e) => setGranularity(e.target.value)}
+              >
+                <Radio.Button value={Granularity.Hour}>Hour</Radio.Button>
+                <Radio.Button value={Granularity.Day}>Day</Radio.Button>
+                <Radio.Button value={Granularity.Week}>Week</Radio.Button>
+                <Radio.Button value={Granularity.Month}>Month</Radio.Button>
+              </Radio.Group>
+            </Col>
+          </Row>
           {metricsData.metrics.length === 0 ? (
             <Empty
               image={Empty.PRESENTED_IMAGE_SIMPLE}
               description="No executions"
             />
           ) : (
-            <>
-              <Row style={{ marginBottom: token.marginLG }}>
-                <Col span={12}>
-                  <Select
-                    defaultValue={start}
-                    style={{ width: 140 }}
-                    onSelect={setStart}
-                  >
-                    <Select.Option value="-1h">Past Hour</Select.Option>
-                    <Select.Option value="-1d">Past Day</Select.Option>
-                    <Select.Option value="-7d">Past 7 days</Select.Option>
-                    <Select.Option value="-30d">Past 30 days</Select.Option>
-                    <Select.Option value="-1y">Past year</Select.Option>
-                  </Select>
-                </Col>
-                <Col
-                  span={12}
-                  style={{ display: "flex", justifyContent: "flex-end" }}
-                >
-                  <Radio.Group
-                    value={granularity}
-                    onChange={(e) => setGranularity(e.target.value)}
-                  >
-                    <Radio.Button value={Granularity.Hour}>Hour</Radio.Button>
-                    <Radio.Button value={Granularity.Day}>Day</Radio.Button>
-                    <Radio.Button value={Granularity.Week}>Week</Radio.Button>
-                    <Radio.Button value={Granularity.Month}>Month</Radio.Button>
-                  </Radio.Group>
-                </Col>
-              </Row>
-              <div>{children}</div>
-            </>
+            <div>{children}</div>
           )}
         </div>
       </Card>
