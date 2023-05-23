b948f4b63d8109008255810be51322a388bb48c2
Itay Elgazar <Itayekk1@gmail.com>
2023-05-23 13:46:24 +0200

    fix: prompt tester to have latest result (#55)
    * fix: cache prompts by project id
    
    * chore: change useCurrentProject file name
    
    * fix: prompt tester modal to show the latest result
    
    * fix: add types
    
    * remove console.log

--- apps/console/src/app/components/prompts/views/PromptEditView.tsx
+++ apps/console/src/app/components/prompts/views/PromptEditView.tsx
@@ -9,6 +9,7 @@ import {
 } from "@ant-design/icons";
 import { css } from "@emotion/css";
 import {
+  PromptEditFormInputs,
   getDraftPromptData,
   usePromptEdit,
 } from "../../../lib/hooks/usePromptEdit";
@@ -33,7 +34,8 @@ export const PromptEditView = () => {
   } = usePromptEdit();
   const { prompt, currentPromptVersion, integration, isDraft } =
     useCurrentPrompt();
-  const { openTester, runTest, isTestInProgress } = usePromptTester();
+  const { openTester, runTest, isTestInProgress, isTesterOpen } =
+    usePromptTester();
   const [isCommitModalOpen, setIsCommitModalOpen] = useState(false);
   const [isPublishModalOpen, setIsPublishModalOpen] = useState(false);
   const [isConsumePromptModalOpen, setIsConsumePromptModalOpen] =
@@ -49,10 +51,10 @@ export const PromptEditView = () => {
     form.resetFields();
   }, [prompt.id, currentPromptVersion]);
 
-  const handleTest = async () => {
+  const handleTest = async (values: PromptEditFormInputs) => {
     await runTest({
-      content: form.getFieldValue("content"),
-      settings: form.getFieldValue("settings"),
+      content: values.content,
+      settings: values.settings,
       variables,
     });
     openTester();
@@ -84,7 +86,7 @@ export const PromptEditView = () => {
           open={isPublishModalOpen}
         />
       )}
-      <PromptTester />
+      {isTesterOpen && <PromptTester />}
 
       <ConsumePromptModal
         open={isConsumePromptModalOpen}
@@ -133,11 +135,11 @@ export const PromptEditView = () => {
               }
             >
               <Button
-                onClick={handleTest}
                 loading={isTestInProgress}
                 icon={<ExperimentOutlined />}
                 disabled={!isTestEnabled}
-                type="default"
+                htmlType="submit"
+                form="prompt-form"
               >
                 Test
               </Button>
@@ -148,10 +150,11 @@ export const PromptEditView = () => {
 
       <Form
         onValuesChange={handleFormValuesChange}
+        onFinish={handleTest}
         initialValues={initialValues}
         form={form}
         layout="vertical"
-        name="basic"
+        name="prompt-form"
         autoComplete="off"
       >
         <Row>
