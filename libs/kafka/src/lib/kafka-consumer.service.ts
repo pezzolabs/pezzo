494ebd7916a0256bda72428e83d0921433a3f79f
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2023-06-18 00:03:08 +0200

    feat: type safety through kafka lib (#89)

--- libs/kafka/src/lib/kafka-consumer.service.ts
+++ libs/kafka/src/lib/kafka-consumer.service.ts
@@ -4,10 +4,11 @@ import {
   OnApplicationShutdown,
   OnModuleInit,
 } from "@nestjs/common";
-import { ConsumerRunConfig } from "kafkajs";
-import { Consumer, ConsumerSubscribeTopics, Kafka } from "kafkajs";
+import { ConsumerRunConfig, EachMessagePayload } from "kafkajs";
+import { Consumer, Kafka } from "kafkajs";
 import { MODULE_OPTIONS_TOKEN } from "./kafka.module-definitions";
 import { KafkaModuleOptions } from "./kafka.module";
+import { KafkaSchemas } from "../schemas";
 
 @Injectable()
 export class KafkaConsumerService
@@ -24,21 +25,33 @@ export class KafkaConsumerService
     return this.connect();
   }
 
-  async connect() {
-    this.kafka = new Kafka(this.kafkaConfig.client);
-  }
-
-  async consume(topics: ConsumerSubscribeTopics, config: ConsumerRunConfig) {
-    const consumer = this.kafka.consumer(this.kafkaConfig.consumer);
-    await consumer.connect();
-    await consumer.subscribe(topics);
-    await consumer.run(config);
-    this.consumers.push(consumer);
-  }
-
   async onApplicationShutdown() {
     for (const consumer of this.consumers) {
       await consumer.disconnect();
     }
+  }
+
+  async connect() {
+    this.kafka = new Kafka(this.kafkaConfig.client);
+  }
+
+  async consume<K extends keyof KafkaSchemas>(
+    topic: K,
+    handler: (data: KafkaSchemas[K]) => Promise<void> | void
+  ): Promise<Consumer> {
+    const consumer = this.kafka.consumer(this.kafkaConfig.consumer);
+    await consumer.connect();
+    await consumer.subscribe({ topic: topic as string });
+
+    const config: ConsumerRunConfig = {
+      eachMessage: async (payload: EachMessagePayload) => {
+        const data = JSON.parse(payload.message.value.toString());
+        await handler(data);
+      },
+    };
+
+    await consumer.run(config);
+    this.consumers.push(consumer);
+    return consumer;
   }
 }
