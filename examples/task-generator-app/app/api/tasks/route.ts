0b2358b411ab3a7286df12d831176a70de4e6f38
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-06-25 16:29:15 +0200

    before functional

--- examples/task-generator-app/app/api/tasks/route.ts
+++ examples/task-generator-app/app/api/tasks/route.ts
@@ -28,8 +28,31 @@ export async function POST(request: Request) {
   let result;
 
   try {
-    settings = prompt.getChatCompletionSettings();
-    result = await openai.createChatCompletion(settings);
+    // settings = prompt.getChatCompletionSettings();
+    // result = await openai.createChatCompletion(settings);
+
+    result = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      max_tokens: 1000,
+      messages: [
+        {
+          role: "user",
+          content: `
+You are a talented task generator.
+
+You'll help me generate tasks to achieve the following goal: "${goal}".
+You must generate exactly ${numTasks} tasks.
+
+You must respond in valid JSON, strictly adhering to the following schema:
+
+{
+  tasks: string[];
+}`
+        }
+      ]
+    });
+
     const parsed = JSON.parse(result.data.choices[0].message.content);
     return NextResponse.json(parsed, {
       headers: { "Content-Type": "application/json" },
