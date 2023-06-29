1b0c147b37a3e33742560bde1abb093354ea343d
Itay Elgazar <Itayekk1@gmail.com>
2023-06-29 15:04:09 +0200

    enable new pezzo client

--- examples/task-generator-app/app/api/tasks/route.ts
+++ examples/task-generator-app/app/api/tasks/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-// import { openai } from "../../lib/pezzo";
-// import { CreatePezzoChatCompletionRequest } from "@pezzo/client";
+import { openai } from "../../lib/pezzo";
+import { CreatePezzoChatCompletionRequest } from "@pezzo/client";
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -9,41 +9,41 @@ export async function POST(request: Request) {
   let result;
 
   try {
-    //   const reqBody: CreatePezzoChatCompletionRequest = {
-    //     pezzo: {
-    //       metadata: {
-    //         conversationId: "task-generator",
-    //       },
-    //       properties: {
-    //         userId: "user-uuid123",
-    //       },
-    //     },
-    //     model: "gpt-3.5-turbo",
-    //     temperature: 0,
-    //     max_tokens: 1000,
-    //     messages: [
-    //       {
-    //         role: "assistant",
-    //         content: `
-    //         You'll help me generate tasks to achieve my goal. You will create exactly ${numTasks} tasks.
+    const reqBody: CreatePezzoChatCompletionRequest = {
+      pezzo: {
+        metadata: {
+          conversationId: "task-generator",
+        },
+        properties: {
+          userId: "user-uuid123",
+        },
+      },
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      max_tokens: 1000,
+      messages: [
+        {
+          role: "assistant",
+          content: `
+            You'll help me generate tasks to achieve my goal. You will create exactly ${numTasks} tasks.
 
-    //         My goal is: ${goal}
+            My goal is: ${goal}
 
-    //         You must respond in valid JSON, strictly adhering to the following schema:
+            You must respond in valid JSON, strictly adhering to the following schema:
 
-    //         {
-    //           tasks: string[];
-    //         }
-    //         `,
-    //       },
-    //     ],
-    //   };
+            {
+              tasks: string[];
+            }
+            `,
+        },
+      ],
+    };
 
-    // result = await openai.createChatCompletion(reqBody);
+    result = await openai.createChatCompletion(reqBody);
 
     // const parsed = JSON.parse(result.data.choices[0].message.content);
     // TODO: this is a temporary place holder for as long as we fix the git state issues
-    const parsed = JSON.parse("{}");
+    const parsed = JSON.parse(result.data.choices[0].text);
     return NextResponse.json(parsed, {
       headers: { "Content-Type": "application/json" },
     });
