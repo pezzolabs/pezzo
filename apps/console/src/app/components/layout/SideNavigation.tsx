0d3e26277a71bcb7432befd79660ffab5373b87c
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-08-20 23:48:20 +0200

    remove overview page

--- apps/console/src/app/components/layout/SideNavigation.tsx
+++ apps/console/src/app/components/layout/SideNavigation.tsx
@@ -1,13 +1,11 @@
 import {
   ChatBubbleBottomCenterIcon,
-  HomeIcon,
 } from "@heroicons/react/24/outline";
 import { Menu } from "antd";
 import { useMemo, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import styled from "@emotion/styled";
 
-import { colors } from "../../lib/theme/colors";
 import { useCurrentProject } from "../../lib/hooks/useCurrentProject";
 import {
   AcademicCapIcon,
@@ -16,11 +14,6 @@ import {
 } from "@heroicons/react/24/outline";
 
 const topMenuItems = [
-  {
-    key: "overview",
-    label: "Overview",
-    icon: <HomeIcon height={18} />,
-  },
   {
     key: "prompts",
     label: "Prompts",
