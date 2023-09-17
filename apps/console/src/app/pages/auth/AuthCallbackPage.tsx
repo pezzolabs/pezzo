769ff127ef0b603f141b1a73112c3dcd36d2ef60
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-09-17 18:28:08 +0200

    fix(console): tracking of signup

--- apps/console/src/app/pages/auth/AuthCallbackPage.tsx
+++ apps/console/src/app/pages/auth/AuthCallbackPage.tsx
@@ -1,13 +1,21 @@
 import { useEffect } from "react";
 import ThirdPartyEmailPassword from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 import { Loader } from "../../components/common/Loader";
+import { trackEvent } from "../../lib/utils/analytics";
 
 export const AuthCallbackPage = () => {
   const handleAuthCallback = async () => {
     try {
       const response = await ThirdPartyEmailPassword.thirdPartySignInAndUp();
+
       if (response.status !== "OK") {
         return window.location.assign("/login?error=signin");
+      }
+
+      if (response.createdNewUser) {
+        trackEvent("user_signup", { method: "third_party" });
+      } else {
+        trackEvent("user_login", { method: "third_party" });
       }
 
       window.location.assign("/");
