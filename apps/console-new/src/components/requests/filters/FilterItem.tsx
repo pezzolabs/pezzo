69b3f164db43e0314c3041dfd78521c1bbd9d8ab
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-24 17:31:12 -0700

    refactor filters to use shadcn

--- apps/console-new/src/components/requests/filters/FilterItem.tsx
+++ apps/console-new/src/components/requests/filters/FilterItem.tsx
@@ -1,25 +1,27 @@
-import styled from "@emotion/styled";
-import { Button, Form, Input, Select, Tag, Tooltip, Typography } from "antd";
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+  SelectItem,
+  Input,
+} from "@pezzo/ui";
 import { useCallback, useMemo, useState } from "react";
-import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
-import { FilterInput } from "~/@generated/graphql/graphql";
+import { FilterInput, FilterOperator } from "~/@generated/graphql/graphql";
 import {
   FILTER_FIELDS_LIST,
   NUMBER_FILTER_OPERATORS,
   STRING_FILTER_OPERATORS,
 } from "~/lib/constants/filters";
-
-const { Text } = Typography;
-
-const FilterItemTag = styled(Tag)<{ formMode?: boolean }>`
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  padding: ${({ formMode }) => (formMode ? "0px" : "4px 8px")};
-  border: ${({ formMode }) => (formMode ? "none" : "1px solid #5FDD97")};
-  background: ${({ formMode }) =>
-    formMode ? "none" : "rgba(255, 255, 255, 0.04)"};
-`;
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CheckIcon, XIcon } from "lucide-react";
 
 interface Props {
   onRemoveFilter: () => void;
@@ -38,7 +40,6 @@ export const FilterItem = ({
   const translatedField = useMemo(() => {
     if (field.includes(".")) {
       const fieldParts = field.split(".");
-
       return fieldParts.reduce((acc, part) => `${acc} ${part}`, "");
     }
 
@@ -46,23 +47,27 @@ export const FilterItem = ({
   }, [field]);
 
   return (
-    <FilterItemTag closable onClose={onRemoveFilter}>
-      <Text>{translatedField}</Text>
-      <Text style={{ fontWeight: "bold" }}>{operator}</Text>
-      <Text>{value}</Text>
-    </FilterItemTag>
+    <div className="flex items-center gap-1 rounded-md border border-primary bg-white px-2 py-1 text-sm">
+      <div>{translatedField}</div>
+      <div>{operator}</div>
+      <div>{value}</div>
+      <button onClick={onRemoveFilter}>
+        <XIcon className="h-4 w-4" />
+      </button>
+    </div>
   );
 };
-
-const StyledForm = styled(Form)`
-  .ant-form-item-explain-error {
-    position: absolute;
-  }
-`;
 
 interface FilterForm extends FilterInput {
   property?: string;
 }
+
+const formSchema = z.object({
+  field: z.string().min(1).max(100),
+  operator: z.nativeEnum(FilterOperator),
+  value: z.string().min(1).max(100),
+  property: z.string().min(1).max(100).optional(),
+});
 
 export const AddFilterForm = ({
   onAdd,
@@ -71,154 +76,141 @@ export const AddFilterForm = ({
   onAdd: (input: FilterInput) => void;
   onCancel: () => void;
 }) => {
-  const [form] = Form.useForm<FilterForm>();
-  Form.useWatch(["field"], form);
-  const field = form.getFieldValue("field");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      field: "",
+      operator: FilterOperator.Eq,
+      value: "",
+      property: undefined,
+    },
+  });
 
-  const [_, setTrigger] = useState(0);
-
-  const handleFormSubmit = () => {
-    const formValues = form.getFieldsValue();
-
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
     const filterValue: FilterInput = {
       field:
-        formValues.field !== "property"
-          ? formValues.field
-          : `properties.${formValues.property}.keyword`,
-      operator: formValues.operator,
-      value: formValues.value,
+        values.field !== "property"
+          ? values.field
+          : `properties.${values.property}.keyword`,
+      operator: values.operator,
+      value: values.value,
     };
 
     onAdd(filterValue);
-    form.resetFields();
+    form.reset();
   };
 
+  const field = form.watch("field");
+  const selectedFilterField = FILTER_FIELDS_LIST.find(
+    (fieldInList) => fieldInList.value === field
+  );
+
+  const isFormValid = form.formState.isValid;
+
   return (
-    <StyledForm
-      form={form}
-      layout="vertical"
-      name="basic"
-      style={{ display: "flex", gap: 8 }}
-      onFinish={handleFormSubmit}
-      autoComplete="off"
-    >
-      <Form.Item
-        name="field"
-        style={{ margin: 0, minWidth: 160 }}
-        rules={[
-          {
-            required: true,
-            type: "string",
-            validateTrigger: "onSubmit",
-            message: "Must be a valid field name",
-          },
-        ]}
-      >
-        <Select placeholder="Select a field" options={FILTER_FIELDS_LIST} />
-      </Form.Item>
-
-      {/*
-        If the user is searching for a custom property, we display an additional field to
-        allow them to specify the property name.
-      */}
-      {field === "property" && (
-        <Form.Item
-          name="property"
-          style={{ margin: 0 }}
-          rules={[
-            {
-              required: true,
-              type: "string",
-              validateTrigger: "onSubmit",
-              message: "Must be a valid property name",
-            },
-          ]}
-        >
-          <Input placeholder="Property name" />
-        </Form.Item>
-      )}
-
-      <Form.Item
-        fieldId="operator"
-        dependencies={["field"]}
-        style={{ margin: 0 }}
-        rules={[
-          {
-            required: true,
-            type: "string",
-            validateTrigger: "onSubmit",
-            message: "Must be a valid operator",
-          },
-        ]}
-      >
-        {({ getFieldValue }) => {
-          const selectedFilterField = FILTER_FIELDS_LIST.find(
-            (field) => field.value === getFieldValue("field")
-          );
-
-          const options =
-            selectedFilterField?.type === "string"
-              ? STRING_FILTER_OPERATORS
-              : NUMBER_FILTER_OPERATORS;
-
-          return (
-            <Form.Item noStyle name="operator">
-              <Select placeholder="Select an operator" options={options} />
-            </Form.Item>
-          );
-        }}
-      </Form.Item>
-
-      <Form.Item
-        name="value"
-        fieldId="value"
-        style={{ margin: 0 }}
-        rules={[
-          {
-            required: true,
-            type: "string",
-            validateTrigger: "onSubmit",
-            message: "Must be a valid value",
-          },
-        ]}
-      >
-        <Input placeholder="Value" />
-      </Form.Item>
-
-      <Form.Item style={{ margin: 0 }} shouldUpdate>
-        {({ getFieldsValue }) => {
-          const { field, operator, value } = getFieldsValue();
-          const fieldsHasError = form
-            .getFieldsError()
-            .some((field) => field.errors.length > 0);
-          const formIsComplete = field && operator && value && !fieldsHasError;
-          return (
-            <Tooltip title="submit">
-              <Button
-                type="primary"
-                htmlType="submit"
-                icon={<CheckOutlined />}
-                disabled={!formIsComplete}
-              />
-            </Tooltip>
-          );
-        }}
-      </Form.Item>
-      <Form.Item style={{ margin: 0 }}>
-        <Tooltip title="cancel">
-          <Button
-            danger
-            type="primary"
-            htmlType="button"
-            onClick={(e) => {
-              e.stopPropagation();
-              onCancel();
-            }}
-            icon={<CloseOutlined />}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="mb-2 text-sm font-semibold">Add Filter</div>
+        <div className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="field"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-40">
+                      <SelectValue placeholder="Field" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {FILTER_FIELDS_LIST.map((field) => (
+                        <SelectItem
+                          key={field.value}
+                          value={field.value}
+                          {...field}
+                        >
+                          {field.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
           />
-        </Tooltip>
-      </Form.Item>
-    </StyledForm>
+          {form.watch("field") === "property" && (
+            <FormField
+              control={form.control}
+              name="property"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Property name" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="operator"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-40">
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {(selectedFilterField?.type === "string"
+                        ? STRING_FILTER_OPERATORS
+                        : NUMBER_FILTER_OPERATORS
+                      ).map((operator) => (
+                        <SelectItem
+                          key={operator.value}
+                          value={operator.value}
+                          {...operator}
+                        >
+                          {operator.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="value"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Value" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button size="icon" type="submit" disabled={!isFormValid}>
+            <CheckIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            type="button"
+            onClick={onCancel}
+          >
+            <XIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 };
 
@@ -242,9 +234,5 @@ export const AddFilterItem = ({
       <AddFilterForm onAdd={handleAdd} onCancel={() => setAddFormOpen(false)} />
     );
   }
-  return (
-    <Button onClick={() => setAddFormOpen(true)} type="dashed">
-      + Add Filter
-    </Button>
-  );
+  return <Button onClick={() => setAddFormOpen(true)}>+ Add Filter</Button>;
 };
--- libs/ui/src/components/ui/input.tsx
+++ libs/ui/src/components/ui/input.tsx
@@ -1,29 +1,29 @@
-import * as React from "react";
+import * as React from "react"
 
-import { cn } from "@pezzo/ui/utils";
+import { cn } from "@pezzo/ui/utils"
 import { cva } from "class-variance-authority";
 
 export interface InputProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
-  size?: "default" | "sm" | "lg";
-}
-
-const inputVariants = cva(
-  "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md bg-neutral-800 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-  {
-    variants: {
-      size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 text-xs",
-        lg: "h-10",
-        icon: "h-9 w-9",
-      },
-    },
-    defaultVariants: {
-      size: "default",
-    },
+    size?: "default" | "sm" | "lg";
   }
-);
+
+  const inputVariants = cva(
+    "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+    {
+      variants: {
+        size: {
+          default: "h-9 px-4 py-2",
+          sm: "h-8 text-xs",
+          lg: "h-10",
+          icon: "h-9 w-9",
+        },
+      },
+      defaultVariants: {
+        size: "default",
+      },
+    }
+  );
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, size, ...props }, ref) => {
@@ -34,9 +34,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    );
+    )
   }
-);
-Input.displayName = "Input";
+)
+Input.displayName = "Input"
 
-export { Input };
+export { Input }
