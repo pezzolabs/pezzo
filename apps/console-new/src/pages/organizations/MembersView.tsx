1b12aaead2f3491334a3ce4239d2b0df8c904085
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-25 20:47:08 -0700

    refactor members view

--- apps/console-new/src/pages/organizations/MembersView.tsx
+++ apps/console-new/src/pages/organizations/MembersView.tsx
@@ -1,12 +1,12 @@
-import { Button, Col, Row, Typography } from "antd";
 import { useCurrentOrganization } from "~/lib/hooks/useCurrentOrganization";
 import { OrgMembersList } from "~/components/organizations/OrgMembersList";
 import { OrgInvitationsList } from "~/components/organizations/OrgInvitationsList";
-import { PlusOutlined } from "@ant-design/icons";
 import { InviteOrgMemberModal } from "~/components/organizations/InviteOrgMemberModal";
 import { useState } from "react";
 import { useCurrentOrgMembership } from "~/lib/hooks/useCurrentOrgMembership";
 import { trackEvent } from "~/lib/utils/analytics";
+import { Button } from "@pezzo/ui";
+import { Plus } from "lucide-react";
 
 export const MembersView = () => {
   const { organization } = useCurrentOrganization({
@@ -33,35 +33,33 @@ export const MembersView = () => {
         open={isInviteModalOpen}
         onClose={() => setIsInviteModalOpen(false)}
       />
-      <div style={{ marginBottom: 20, maxWidth: 800 }}>
-        <Row align="middle">
-          <Col flex="1">
-            <Typography.Title level={2}>
-              Members ({members.length})
-            </Typography.Title>
-          </Col>
-          {isOrgAdmin && (
-            <Col>
-              <Button
-                onClick={onOpenInviteModal}
-                type="primary"
-                icon={<PlusOutlined />}
-              >
+
+      <div className="max-w-[800px]">
+        <div className="mb-2 flex items-center gap-4">
+          <h2 className="flex-1 text-2xl font-semibold">
+            Members ({members.length})
+          </h2>
+          <div>
+            {isOrgAdmin && (
+              <Button onClick={onOpenInviteModal}>
+                <Plus className="mr-2 h-4 w-4" />
                 Invite Member
               </Button>
-            </Col>
-          )}
-        </Row>
-        <OrgMembersList members={members} />
-      </div>
-      {isOrgAdmin && invitations.length > 0 && (
-        <div style={{ marginBottom: 20, maxWidth: 800 }}>
-          <Typography.Title level={2}>
-            Pending Invitations ({invitations.length})
-          </Typography.Title>
-          <OrgInvitationsList invitations={invitations} />
+            )}
+          </div>
         </div>
-      )}
+
+        <OrgMembersList members={members} />
+        {isOrgAdmin && invitations.length > 0 && (
+          <div className="mt-4">
+            <h2 className="text-2xl font-semibold">
+              Pending Invitations ({invitations.length})
+            </h2>
+
+            <OrgInvitationsList invitations={invitations} />
+          </div>
+        )}
+      </div>
     </>
   );
 };
