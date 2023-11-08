4697eed6b31d825606936fc5ca1045d603ec4faa
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-11-07 23:53:10 -0800

    feat(console): add api keys to project sidenav

--- apps/console/src/components/layout/SideNavigation.tsx
+++ apps/console/src/components/layout/SideNavigation.tsx
@@ -3,6 +3,7 @@ import {
   BoxIcon,
   GraduationCapIcon,
   HardDriveIcon,
+  KeyRoundIcon,
   RadioIcon,
 } from "lucide-react";
 import { cn } from "@pezzo/ui/utils";
@@ -10,10 +11,12 @@ import { useCurrentProject } from "~/lib/hooks/useCurrentProject";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useCurrentOrganization } from "~/lib/hooks/useCurrentOrganization";
 
 export const SideNavigation = () => {
   const [collapsed, setCollapsed] = useState(true);
   const { projectId } = useCurrentProject();
+  const { organizationId } = useCurrentOrganization();
 
   const projectNavigation = [
     {
@@ -87,6 +90,25 @@ export const SideNavigation = () => {
             </ul>
           </li>
           <li className="mt-auto">
+            <Link
+              className="mb-2 mt-auto flex cursor-pointer items-center rounded-md p-2 text-sm font-medium text-stone-400 transition-all hover:bg-stone-800 hover:text-white "
+              to={`/orgs/${organizationId}/api-keys`}
+            >
+              <KeyRoundIcon className="h-5 w-5 shrink-0" aria-hidden="true" />
+
+              <motion.div
+                initial={{ width: 0, opacity: 0, marginLeft: 0 }}
+                animate={{
+                  width: collapsed ? 0 : "auto",
+                  opacity: collapsed ? 0 : 1,
+                  marginLeft: collapsed ? 0 : 10,
+                }}
+                exit={{ width: 0, opacity: 0, marginLeft: 0 }}
+                className="whitespace-nowrap"
+              >
+                API Keys
+              </motion.div>
+            </Link>
             <Link
               className="mb-2 mt-auto flex cursor-pointer items-center rounded-md p-2 text-sm font-medium text-stone-400 transition-all hover:bg-stone-800 hover:text-white "
               to="https://docs.pezzo.ai"
