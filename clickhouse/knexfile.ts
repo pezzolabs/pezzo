4e694f22c82dc034bba56950a2fad640f7ecdffd
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-12-31 10:59:35 -0800

    test failing migration script

--- clickhouse/knexfile.ts
+++ clickhouse/knexfile.ts
@@ -2,13 +2,15 @@ import { Knex } from "knex";
 import clickhouse from "@pezzo/knex-clickhouse-dialect";
 
 const {
+  CLICKHOUSE_PROTOCOL = "http",
   CLICKHOUSE_HOST = "localhost",
   CLICKHOUSE_PORT = "8123",
   CLICKHOUSE_USER = "default",
   CLICKHOUSE_PASSWORD = "default",
+  CLICKHOUSE_DATABASE = "default",
 } = process.env;
 
-const str = `https://${CLICKHOUSE_USER}:${CLICKHOUSE_PASSWORD}@${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT}/default`;
+const str = `${CLICKHOUSE_PROTOCOL}://${CLICKHOUSE_USER}:${CLICKHOUSE_PASSWORD}@${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT}/${CLICKHOUSE_DATABASE}`;
 
 const config: { [key: string]: Knex.Config } = {
   default: {
