94b6181a4ea7d4e2ec42ee368fd9c5d6bd174d5a
Frank-Xie-SmartNews <frank.xie@smartnews.com>
2024-04-24 16:25:11 +0800

    add create table DDL

---
+++ clickhouse/migrations/create_report.sql
@@ -0,0 +1,35 @@
+create table default.reports on cluster hacluster
+as default.reports_local
+  engine = Distributed('{cluster}', 'default', 'reports_local', rand());
+CREATE TABLE default.reports_local on cluster hacluster
+(
+  `id` String,
+  `timestamp` DateTime,
+  `environment` String,
+  `organizationId` String,
+  `projectId` String,
+  `promptTokens` Float64,
+  `completionTokens` Float64,
+  `totalTokens` Float64,
+  `promptCost` Float64,
+  `completionCost` Float64,
+  `totalCost` Float64,
+  `duration` UInt32,
+  `type` String,
+  `client` String,
+  `clientVersion` String,
+  `model` String,
+  `provider` String,
+  `modelAuthor` String,
+  `requestTimestamp` DateTime,
+  `requestBody` String,
+  `isError` Bool,
+  `responseStatusCode` UInt32,
+  `responseTimestamp` DateTime,
+  `responseBody` String,
+  `cacheEnabled` Bool,
+  `cacheHit` Bool
+)
+  ENGINE = ReplicatedMergeTree()
+  ORDER BY tuple()
+  SETTINGS index_granularity = 8192
