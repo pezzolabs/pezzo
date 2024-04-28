0c1b21e488efe795aae25c21a5e822520df3ea0a
Ariel Weinberger <ariel@pezzo.ai>
2024-04-28 15:10:35 +0200

    fix: trivial cleanup (#298)
    * fix: trivial cleanup
    
    * fix: linting

--- libs/common/src/utils/slugify.ts
+++ libs/common/src/utils/slugify.ts
@@ -1,5 +1,5 @@
 export function slugify(str: string) {
-  str = str.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
+  str = str.replace(/(^\s+|\s+$)/g, ""); // trim leading/trailing white space
   str = str.toLowerCase(); // convert string to lowercase
   str = str
     .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
