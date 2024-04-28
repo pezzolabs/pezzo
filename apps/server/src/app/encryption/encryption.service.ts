5d585e4b3a8ce13548c2e12cc89a33479678b51f
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-04-28 14:13:09 +0200

    fix(encryption): change encryption method to AES-GCM

--- apps/server/src/app/encryption/encryption.service.ts
+++ apps/server/src/app/encryption/encryption.service.ts
@@ -48,25 +48,28 @@ export class EncryptionService {
   async encrypt(data: string): Promise<{
     encryptedData: string;
     encryptedDataKey: string;
+    tag: string; // Include the authentication tag in the encryption result
   }> {
     this.logger.info("Encrypting data");
 
     const dataKey = await this.generateDataKey();
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", dataKey.plaintext, iv);
-    const encrypted = Buffer.concat([
-      cipher.update(data, "utf8"),
-      cipher.final(),
-    ]);
-    const encryptedData = Buffer.concat([iv, encrypted]);
+    const iv = crypto.randomBytes(12); // 12 bytes is recommended for GCM
+    const cipher = crypto.createCipheriv("aes-256-gcm", dataKey.plaintext, iv);
+    let encrypted = cipher.update(data, "utf8");
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
 
     return {
-      encryptedData: encryptedData.toString("hex"),
+      encryptedData: Buffer.concat([iv, encrypted]).toString("hex"),
       encryptedDataKey: Buffer.from(dataKey.ciphertext).toString("base64"),
+      tag: cipher.getAuthTag().toString("hex"), // Store the tag for verification during decryption
     };
   }
 
-  async decrypt(encryptedData: string, dataKeyBase64: string): Promise<string> {
+  async decrypt(
+    encryptedData: string,
+    dataKeyBase64: string,
+    tagHex: string
+  ): Promise<string> {
     this.logger.info("Decrypting data");
 
     const encryptedDataBuffer = Buffer.from(encryptedData, "hex");
@@ -74,9 +77,11 @@ export class EncryptionService {
       CiphertextBlob: Buffer.from(dataKeyBase64, "base64"),
     });
 
-    const iv = encryptedDataBuffer.slice(0, 16);
-    const data = encryptedDataBuffer.slice(16);
-    const decipher = crypto.createDecipheriv("aes-256-cbc", dataKey, iv);
+    const iv = encryptedDataBuffer.slice(0, 12);
+    const data = encryptedDataBuffer.slice(12);
+    const decipher = crypto.createDecipheriv("aes-256-gcm", dataKey, iv);
+    decipher.setAuthTag(Buffer.from(tagHex, "hex")); // Set the authentication tag for verification
+
     return decipher.update(data) + decipher.final("utf8");
   }
 }
