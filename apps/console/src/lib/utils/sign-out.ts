4fabd90d158ea814c6a45ca036f188fa842da026
Frank-Xie-SmartNews <frank.xie@smartnews.com>
2024-05-13 17:59:24 +0800

    Clear all local storage when login out to avoid use old user id when login again

--- apps/console/src/lib/utils/sign-out.ts
+++ apps/console/src/lib/utils/sign-out.ts
@@ -3,6 +3,7 @@ import { trackEvent } from "./analytics";
 
 export async function signOut() {
   trackEvent("user_logout");
+  localStorage.clear();  // Clear all local storage when login out to avoid use old user id when login again
   await supertokensSignOut();
   window.location.href = "/login";
 }
