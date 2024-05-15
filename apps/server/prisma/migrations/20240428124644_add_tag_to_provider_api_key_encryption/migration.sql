6dfeabbee4deeda4333ba355dfa546496e9499c7
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-05-15 14:58:34 +0200

    hotfix: api key encrypted migrations

--- apps/server/prisma/migrations/20240428124644_add_tag_to_provider_api_key_encryption/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `encryptionTag` to the `ProviderApiKey` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "ProviderApiKey" ADD COLUMN     "encryptionTag" TEXT NOT NULL;
