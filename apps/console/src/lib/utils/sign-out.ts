88aff624dfa7825489350ac76ac1352def2b4c61
Frank-Xie-SmartNews <frank.xie@smartnews.com>
2024-05-16 18:49:04 +0800

    remove FE authN with supertoken

--- apps/console/src/lib/utils/sign-out.ts
+++ apps/console/src/lib/utils/sign-out.ts
@@ -4,6 +4,6 @@ import { trackEvent } from "./analytics";
 export async function signOut() {
   trackEvent("user_logout");
   localStorage.clear();  // Clear all local storage when login out to avoid use old user id when login again
-  await supertokensSignOut();
+  // await supertokensSignOut();
   window.location.href = "/login";
 }
